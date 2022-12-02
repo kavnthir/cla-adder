ac03ba4af25c47d1dab641507db8f0bd4049b39b
Max2761 <wmax@vt.edu>
2022-12-01 20:42:49 -0500

    Revised sqrt

--- sqrt.v
+++ sqrt.v
@@ -1,47 +1,38 @@
 
-module sqrt (radicand, root, clk, enable, reset, valid_bit);
+module sqrt (radicand, root, enable,valid_bit);
 
 	input [7:0] radicand;
-	input clk, enable, reset;
+	input enable;
 	output [7:0] root;
 	output valid_bit;
 	
 	reg [7:0] root;
 	reg valid_bit;
-	integer value;
+	reg [7:0] i;
 	reg [7:0] square;
 	
-	always @(clk or enable or reset)begin
 	
-	if(!reset)begin
-	root = 0;
-	valid_bit = 1;
-	end
+	always @(radicand)begin
+	if(enable)begin
 	
-	else if(enable)begin
-	for(value = 0; value <= 16; value = value + 1)begin
-	square = value * value;
-	
+	for(i = 0; i <= 8'd16; i = i + 1)begin
+	square = i * i;
 	if(square == radicand)begin
-	root = value;
-	value = 16;
+	root = i;
 	valid_bit = 1;
 	end
-	
-	if(square > radicand)begin
-	root = value;
-	value = 16;
+	else if(square > radicand)begin
+	root = i - 1;
+	i = i + 16;
 	valid_bit = 0;
 	end
-	end	
+	end
 	end
 	
 	else begin
-	root[7:0] = 8'bx;
+	root = 0;
 	end
 	
-	
 	end
-	
 
 endmodule
--- sqrt_tb.v
+++ sqrt_tb.v
@@ -1,23 +1,18 @@
 module sqrt_tb();
-	reg clk, enable, reset;
+	reg enable;
 	reg [7:0] radicand;
 	reg [8:0] count;
 	wire [7:0] root;
 	wire valid;
-	sqrt U1(radicand, root, clk, enable, reset, valid);
+	sqrt U1(radicand, root,enable, valid);
 	
-	wire clock_out;
-	clk #(20) M2 (enable, clock_out);
-	always @(posedge clock_out or negedge clock_out)begin
-	clk = clock_out;
-	end
+	
+	
 	
 	initial begin
-	reset = 0;
 	enable = 0;
 	#50;
 	enable = 1;
-	reset = 1;
 	#50;
 		for(count = 0; count < 9'b100000000; count = count + 1)begin
 		radicand = count[7:0];
@@ -29,20 +24,3 @@ module sqrt_tb();
 
 endmodule
 
-module clk(enable, clk_out);
-	parameter PERIOD = 50;		// The default period of the clock
-	input enable;				// Allow clk_out to "run" when asserted
-	output clk_out;				// Controlled clock out
-
-	reg clk_out;					// Declare clk_out as a reg
-
-	// Set initial value for clk_out on power-up
-	initial clk_out = 0;
-
-	// Produce controlled free-running clock
-	always
-	begin
-		#(PERIOD/2) if (enable == 1) 
-			clk_out = ~clk_out;
-	end
-endmodule
